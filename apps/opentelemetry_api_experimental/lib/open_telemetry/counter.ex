3872f5943959e95f0575c488f8ce56b045afcd96
Brian Underwood <brian@brian-underwood.codes>
2025-05-20 16:17:12 +0200

    Update counter.ex

--- apps/opentelemetry_api_experimental/lib/open_telemetry/counter.ex
+++ apps/opentelemetry_api_experimental/lib/open_telemetry/counter.ex
@@ -5,8 +5,10 @@ defmodule OpenTelemetryAPIExperimental.Counter do
 
   defmacro create(name, opts) do
     quote bind_quoted: [name: name, opts: opts] do
+      {_meter_module, meter} = :opentelemetry_experimental.get_meter(:opentelemetry.get_application_scope(__MODULE__)),
+
       :otel_meter.create_counter(
-        :opentelemetry_experimental.get_meter(:opentelemetry.get_application_scope(__MODULE__)),
+        meter,
         name,
         opts
       )
@@ -15,9 +17,11 @@ defmodule OpenTelemetryAPIExperimental.Counter do
 
   defmacro add(name, number, attributes) do
     quote bind_quoted: [name: name, number: number, attributes: attributes] do
+      {_meter_module, meter} = :opentelemetry_experimental.get_meter(:opentelemetry.get_application_scope(__MODULE__)),
+
       :otel_counter.add(
         OpenTelemetry.Ctx.get_current(),
-        :opentelemetry_experimental.get_meter(:opentelemetry.get_application_scope(__MODULE__)),
+        meter,
         name,
         number,
         attributes
